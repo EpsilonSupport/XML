a21f96b6852382160f1def5fb8cc82590e58bc01
EpsilonSupport <63876888+EpsilonSupport@users.noreply.github.com>
2023-03-01 12:25:06 -0500

    Add files via upload

---
+++ TEVET-CheckNetwork-ConnectAlwaysOnVPN.ps1
@@ -0,0 +1,12 @@
+﻿Start-Sleep -Seconds 10
+try
+{
+  # Check if computer is connected to domain network
+  [void]::([System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain())
+
+  Write-Host 'Domain Network'
+}
+catch
+{
+    rasdial.exe "TEVET Always-On VPN"
+}
